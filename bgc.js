8658f2a622a1e53d16fdb8c1c06a76c241901e84
anaiah <carlodominguez@yahoo.com>
2026-04-15 19:52:28 +0800

    Update bgc.js

--- bgc.js
+++ bgc.js
@@ -86,7 +86,7 @@ app.use(function(req, res, next) {
 */
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain https://ccfbgc.org for localhost accept *
+    res.header('Access-Control-Allow-Origin', 'https://ccfbgc.org'); // restrict it to the required domain https://ccfbgc.org for localhost accept *
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type,Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
