3f625e30d440706fc0ba4c0a95129c7be29b977d
anaiah <carlodominguez@yahoo.com>
2026-04-15 22:51:31 +0800

    apr15v17

--- bgc.js
+++ bgc.js
@@ -70,6 +70,28 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 // }))
 
+app.use((req, res, next) => {
+    const allowedOrigins = ['https://ccfbgc.org', 'https://www.ccfbgc.org'];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true'); // Required if sending cookies/tokens
+
+    // Immediately respond to preflight OPTIONS requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    
+    next();
+});
+
+
+
 //======== END NODEJS CORS SETTING
 const getRandomPin = (chars, len)=>[...Array(len)].map(
     (i)=>chars[Math.floor(Math.random()*chars.length)]
